6dda9078b6c680951ccce4a6f7fedb4533506fc9
werdeshka <45660274+werdeshka@users.noreply.github.com>
2018-12-06 16:50:11 +0300

    Add files via upload

---
+++ 2.JavaCore/task16/task1602/Solution.java
@@ -0,0 +1,13 @@
+package com.javarush.task.task16.task1602;
+
+/* 
+My second thread
+*/
+/*
+public class Solution {
+    public static void main(String[] args) {
+        TestThread thread = new TestThread();
+        thread.start();
+    }
+}
+*/
